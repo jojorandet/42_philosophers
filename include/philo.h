25549b22fd2076551fe48019fb7fbf1a61e7caf1
Jojorandet <josephine@randet.com>
2025-05-01 14:26:03 +0200

    timelapsed function

--- include/philo.h
+++ include/philo.h
@@ -6,7 +6,7 @@
 /*   By: jrandet <jrandet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 18:45:01 by jrandet           #+#    #+#             */
-/*   Updated: 2025/05/01 13:29:10 by jrandet          ###   ########.fr       */
+/*   Updated: 2025/05/01 13:43:07 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <string.h>
+
 
 int	main(int argc, char **argv);
 //usleep uses unsigned int uscs, and then usleep(usecs)
--- src/main.c
+++ src/main.c
@@ -6,25 +6,41 @@
 /*   By: jrandet <jrandet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 18:42:34 by jrandet           #+#    #+#             */
-/*   Updated: 2025/05/01 13:23:44 by jrandet          ###   ########.fr       */
+/*   Updated: 2025/05/01 14:21:10 by jrandet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  "philo.h"
 
+
+unsigned long	calculate_elapsed_time(void)
+{
+	struct	timeval	start;
+	struct	timeval	end;
+	unsigned long	elapsed_usec;	
+
+	gettimeofday(&start, 0);
+	sleep(1);
+	gettimeofday(&end, 0);
+	elapsed_usec = ((end.tv_sec * 1e6) + end.tv_usec) - ((start.tv_sec * 1e6) + start.tv_usec);
+	return (elapsed_usec);
+}
+
 int main(int argc, char **argv)
 {
-	struct	timeval tv;
 
-
-	if (argc != 6)
-		return (1);
+	(void)argc;
+	//if (argc != 6)
+		//return (1);
 	(void)argv;
-	printf("hello\n");
-	gettimeofday(&tv, NULL);
-	printf("seconds since 1/1/1970: %lu\n", tv.tv_sec);
-	printf("Microseconds: %lds\n", tv.tv_usec);
-	printf("time passes ...\n");
-	printf()
+	printf("Philosophers has started...\n\n");
+	printf("%lu Philosopher 1 has taken a fork\n", calculate_elapsed_time());
+	printf("%lu Philosopher 1 has taken a fork\n", calculate_elapsed_time());
+	printf("%lu Philosopher 1 is eating\n", calculate_elapsed_time());
+	printf("%lu Philosopher 1 is sleeping\n", calculate_elapsed_time());
+	printf("%lu Philosopher 1 had died\n", calculate_elapsed_time());
+
 	return (0);
 }
+
+//everytime i will print, i need to get the time elapsed since that point in time 
